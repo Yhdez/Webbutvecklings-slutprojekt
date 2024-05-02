328f3283e3565b9ab9afe9138f1af58e29e1cdc5
Alexander Yhde <Alexander.yhde@skola.taby.se>
2024-05-02 10:51:14 +0200

    Version 2

---
+++ Top_meny.js
@@ -0,0 +1,4 @@
+var Home_page = document.getElementById("Home_page")
+var Search_movies_page = document.getElementById("Search_movies_page")
+var Contact_information_page = document.getElementById("Contact_information_page")
+
